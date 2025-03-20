afefddbab4863e272e9d17e3b3938d844d3ce4e4
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-03-20 22:00:18 +0100

    add header

--- src/components/dcComicsHeader.jsx
+++ src/components/dcComicsHeader.jsx
@@ -4,19 +4,89 @@ function DcHeader() {
 
     return (
         <header>
-            <img src={DcLogo} alt="" />
+            <img height={82} src={DcLogo} alt="" />
 
             <ul id='site_map'>
-                <li>CHARACTERS</li>
-                <li>COMICS</li>
-                <li>MOVIES</li>
-                <li>TV</li>
-                <li>GAMES</li>
-                <li>COLLECTIBLES</li>
-                <li>VIDEOS</li>
-                <li>FANS</li>
-                <li>NEWS</li>
-                <li>SHOP</li>
+                <li> CHARACTERS
+                    <div className='highlight_bar'></div>
+                    <div className='dropdown_content'>
+                        <div className="page_margins">
+                            <h1>--&gt; Content goes here &lt;--</h1>
+                        </div>
+                    </div>
+                </li>
+                <li>COMICS
+                    <div className='highlight_bar'></div>
+                    <div className='dropdown_content'>
+                        <div className="page_margins">
+                            <h1>--&gt; Content goes here &lt;--</h1>
+                        </div>
+                    </div>
+                </li>
+                <li>MOVIES
+                    <div className='highlight_bar'></div>
+                    <div className='dropdown_content'>
+                        <div className="page_margins">
+                            <h1>--&gt; Content goes here &lt;--</h1>
+                        </div>
+                    </div>
+                </li>
+                <li>TV
+                    <div className='highlight_bar'></div>
+                    <div className='dropdown_content'>
+                        <div className="page_margins">
+                            <h1>--&gt; Content goes here &lt;--</h1>
+                        </div>
+                    </div>
+                </li>
+                <li>GAMES
+                    <div className='highlight_bar'></div>
+                    <div className='dropdown_content'>
+                        <div className="page_margins">
+                            <h1>--&gt; Content goes here &lt;--</h1>
+                        </div>
+                    </div>
+                </li>
+                <li>COLLECTIBLES
+                    <div className='highlight_bar'></div>
+                    <div className='dropdown_content'>
+                        <div className="page_margins">
+                            <h1>--&gt; Content goes here &lt;--</h1>
+                        </div>
+                    </div>
+                </li>
+                <li>VIDEOS
+                    <div className='highlight_bar'></div>
+                    <div className='dropdown_content'>
+                        <div className="page_margins">
+                            <h1>--&gt; Content goes here &lt;--</h1>
+                        </div>
+                    </div>
+                </li>
+                <li>FANS
+                    <div className='highlight_bar'></div>
+                    <div className='dropdown_content'>
+                        <div className="page_margins">
+                            <h1>--&gt; Content goes here &lt;--</h1>
+                        </div>
+                    </div>
+                </li>
+                <li>NEWS
+                    <div className='highlight_bar'></div>
+                    <div className='dropdown_content'>
+                        <div className="page_margins">
+                            <h1>--&gt; Content goes here &lt;--</h1>
+                        </div>
+                    </div>
+                </li>
+                <li>SHOP
+                    <div className='highlight_bar'></div>
+                    <div className='dropdown_content'>
+                        <div className="page_margins">
+                            <h1>--&gt; Content goes here &lt;--</h1>
+                        </div>
+                    </div>
+                </li>
             </ul>
         </header>
 
